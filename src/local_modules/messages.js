6774aa1b4b572ff95e515aecca85754fc8e7b24f
Peter Vertesi <vpeter1119@gmail.com>
2020-02-18 16:30:17 +0100

    Restructured content. Added SendApproveMsg

--- src/local_modules/messages.js
+++ src/local_modules/messages.js
@@ -3,6 +3,7 @@ const gmUser = process.env.GMAIL_USER;
 const gmClientId = process.env.GOOGLE_CID;
 const gmClientSecret = process.env.GOOGLE_CS;
 const gmRefreshToken = process.env.GOOGLE_RT;
+const endUserEmail = process.env.ENDUSER_EMAIL;
 
 //Configure SMTP transport
 const transporter = nodemailer.createTransport({
@@ -16,17 +17,11 @@ const transporter = nodemailer.createTransport({
 	}
 });
 
-exports.SendTestMsg = function SendTestMsg() {
-	//Configure message data
-	var msgData = {
-		from: gmUser,
-		to: gmUser,
-		subject: 'Test email from Bíborrezsi nodejs server',
-		text: 'This email serves to test the nodemailer functionalities.'
-	};
+//Message send function (Common)
+function SendMessage(msgData){
 	
-	//Send the message
 	transporter.sendMail(msgData, (error,info) => {
+		console.log("DEVLOG: Sending message to " + msgData.to);
 		if (error) {
 			console.log(error);
 			console.log("DEVLOG: Message was not sent.");
@@ -34,4 +29,32 @@ exports.SendTestMsg = function SendTestMsg() {
 			console.log("DEVLOG: Message sent to " + msgData.to + ". Response: " + info.response);
 		}
 	});
+};
+
+//Test message function (Exported)
+exports.SendTestMsg = function SendTestMsg() {
+	//Configure message data
+	var msgData = {
+		from: gmUser,
+		to: gmUser,
+		subject: "Test email from Bíborrezsi nodejs server",
+		text: "This email serves to test the nodemailer functionalities."
+	};
+	
+	//Send the message
+	SendMessage(msgData);
+}
+
+//Aprove message function (Exported)
+exports.SendApproveMsg = function SendApproveMsg(reportData, reportId, approveToken) {
+	//Configure message data
+	var msgData = {
+		from: gmUser,
+		to: gmUser,
+		subject: "[Biborrezsi Server] New report on Bíbor Rezsi!",
+		html: '<html><body><p>User has submitted a new report to Bíborrezsi Server:</p><h4>Report report_id</h4><table><thead><th>Név</th><th>Állás</th></thead><tbody><tr><td>Hidegvíz</td><td>'+ reportData.cold +'</td></tr></tbody></table><p>Click on the link below to approve the report:</p><p><a href="https://biborrezsi-server.herokuapp.com/api/report?id=report_id&a=approve&t=approve_token">[APPROVE]</a></p></body></html>'
+	};
+	
+	//Send the message
+	SendMessage(msgData);
 }
