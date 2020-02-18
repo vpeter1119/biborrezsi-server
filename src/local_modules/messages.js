6b592dda25f728f308f63122833efb2d967d6865
Peter Vertesi <vpeter1119@gmail.com>
2020-02-18 10:02:25 +0100

    Added local_modules folder and messages.js

---
+++ src/local_modules/messages.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+const gmUser = process.env.GMAIL_USER;
+const gmPass = process.env.GMAL_APP_PW;
+
+//Configure SMTP transport
+const transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: gmUser,
+		pass: gmPass
+	}
+});
+
+module.exports = function SendTestMsg() {
+	//Configure message data
+	var msgData = {
+		from: gmUser,
+		to: gmUser,
+		subject: 'Test email from Bíborrezsi nodejs server',
+		text: 'This email serves to test the nodemailer functionalities.'
+	};
+	
+	//Send the message
+	transporter.sendMail(msgData, (error,info) => {
+		if (error) {
+			console.log(error);
+			console.log("DEVLOG: Message was not sent.");
+		} else {
+			console.log("DEVLOG: Message sent to " + msgData.to + ". Response: " + info.response);
+		}
+	});
+}
