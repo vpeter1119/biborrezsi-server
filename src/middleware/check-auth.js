3d16f3c4ed8d6260e6d1267384c9a380c4570f7b
Peter Vertesi <vpeter1119@gmail.com>
2020-02-20 14:47:35 +0100

    Added new middleware: check-auth

---
+++ src/middleware/check-auth.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.userData = {
+      username: decodedToken.username,
+      isAdmin: decodedToken.isAdmin
+    };
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Authentication failed. Please log in." });
+  }
+};
