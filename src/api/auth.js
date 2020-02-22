bf3219dec47eb72f0a5432a7e3bdb5809e9fda3f
vpeter1119 <vpeter1119@gmail.com>
2020-02-22 13:14:11 +0100

    Introduced promise to handle async authentication. Corrections to previous code.

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,53 +1,72 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const User = require("../../models/user");
+const User = require("../models/user.js");
 const jwtSecret = process.env.JWT_SECRET;
 
 //Authentication with jsonwebtoken
 router.post("/login", (req, res, next) => {
-	const userData;
+	var reqUser = req.body.username;
+	console.log("Authentication request, username=" + reqUser);
+	var userData = {};
 	//The password from the frontend form	
-	let inputPW = req.body.pw;	
-	//The password from the database
-	let storedPW = User.findOne({username:req.body.username}, user => {
-		if (user) {
-			userData = user;
-			return user.password;
-		} else {
-			console.log("DEVLOG: Authentication failed: could not find user with username " + req.body.username);
-			res.status(500).json({
-				errcode:"ERR",
-				message:"Could not authenticate due to server-side error. Please try again later, or contact me."
+	var inputPW = req.body.pw;
+	console.log("inputPW=" + inputPW);	
+	
+	//Get user data from DB for password compare
+	var promiseGetUserData = new Promise((resolve, reject) => {
+		User.findOne({'username':reqUser}, (err,user) => {
+			if (err) {
+				console.log("Other error:")
+				console.log(err);
+				res.status(500).json({
+					errcode:"ERR",
+					message:"Could not authenticate due to server-side error. Please try again later, or contact me."
+				});
+			} else if (user) {
+				userData = user;
+				setTimeout( ()=> {
+					resolve(userData);
+				}, 250);
+			} else {
+				console.log("DEVLOG: Authentication failed: could not find user with username " + reqUser);
+				res.status(500).json({
+					errcode:"ERR",
+					message:"Could not authenticate due to server-side error. Please try again later, or contact me."
+				});
+			}
+		});	
+	});
+	
+	//Compare passwords after getting user data
+	promiseGetUserData.then((fetchedData) => {
+		if (inputPW == fetchedData.password) {
+			console.log("DEVLOG: Authentication successful.");
+			//Create token
+			const token = jwt.sign(
+				{
+					username: reqUser,
+					isAdmin: userData.isAdmin,
+				},
+				jwtSecret,
+				{expiresIn:"4h"}
+			);
+			//Send response with token
+			res.status(200).json({
+				token: token,
+				expiresIn: 14400,
+				message: "Authentication successful."
 			});
-		}		
-	});	
-	//Compare passwords
-	if (inputPW === storedPW) {
-		console.log("DEVLOG: Authentication successful.");
-		//Create token
-		const token = jwt.sign(
-			{
-				username: req.body.username,
-				isAdmin: userData.isAdmin,
-			},
-			jwtSecret,
-			{expiresIn:"4h"}
-		);
-		//Send response with token
-		res.status(200).json({
-			token: token,
-			expiresIn: 14400,
-			message: "Authentication successful."
-		});
-	} else {
-		console.log("DEVLOG: Authentication failed: incorrect password.");
-		//Send response
-		res.status(401).json({
-			errcode: "WRONGPW",
-			message: "Authentication failed: wrong password. Please note that the password is case-sensitive."
-		});
-	}
+		} else {
+			console.log("DEVLOG: Authentication failed: incorrect password.");
+			//Send response
+			res.status(401).json({
+				errcode: "WRONGPW",
+				message: "Authentication failed: wrong password. Please note that the password is case-sensitive."
+			});
+		}
+	});
+	
 });
 
 //Export routes
