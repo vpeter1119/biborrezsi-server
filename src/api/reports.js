3afe38db029b0f5d74fd8d312463fbe4fbdebca7
vpeter1119 <vpeter1119@gmail.com>
2020-02-24 21:26:28 +0100

    Added input validation to reports POST route

--- src/api/reports.js
+++ src/api/reports.js
@@ -5,6 +5,8 @@ var rs = require("randomstring");
 const Report = require("../models/report.js");
 const checkAuth = require("../middleware/check-auth");
 
+var allReports = [{}];
+
 //Report route functions
 function GetAllReports() {
 	return Report.find({}, (err, reports) => {
@@ -14,7 +16,6 @@ function GetAllReports() {
 			return(null);
 		} else {
 			console.log("DEVLOG: Success.");
-			console.log(reports);
 			return(reports);
 		}
 	});
@@ -29,17 +30,15 @@ router.get("", checkAuth, (req, res, next) => {
 		var reportsList = GetAllReports();
 		setTimeout(()=>{
 			resolve(reportsList);
-		}, 300);
+		}, 250);
 	});
 	GetAllReportsPromise.then(reportsList => {
 		if (reportsList==null) {
-			console.log("DEVLOG: reports list is " + reportsList);
 			res.status(500).json({message:"Something went wrong."});
 		} else {
 			var responseDataRaw = reportsList.filter(report => {
 				return report.isApproved == true;
 			});
-			console.log("DEVLOG: " + responseDataRaw);
 			var responseData = responseDataRaw.map( function (rep) {
 				return {
 					cold: rep.cold,
@@ -50,61 +49,74 @@ router.get("", checkAuth, (req, res, next) => {
 					nr: rep.nr,
 				}
 			});
-			console.log("DEVLOG: " + responseData);
 			res.status(200).json(responseData);
 		}
 	});
 });
 
 router.post("", checkAuth, (req, res, next) => {
-  var origin = req.get('origin');
-  console.log("DEVLOG: Reports POST request from: " + origin);
-  var input = req.body;
-  //Send input for validation before doing anything else
-  var inputValidated = new Promise((resolve,reject) => {
-	  var isValidated = validateInput(input);
-	  if (isValidated) {
-		  resolve(input);
-	  } else {
-		  reject();
-	  }
-  });
-  inputValidated.then(vInput => {
-	//Process the validated input
-	var newHeat = 0;
-	if (vInput.heat == 0 || vInput.heat == null) {
-		//heat should be set to value in latest report
-	} else {
-		newHeat = vInput.heat;
-	}
-	var newReport = {
-		cold: vInput.cold,
-		hot: vInput.hot,
-		heat: vInput.heat,
-		elec: vInput.elec,
-		isHeating: vInput.isHeating,
-		isApproved: false,
-	};
-	//Set "nr" attribute
-	//Set approve token
-	newReport.approveToken = rs.generate();
-	console.log(newReport);
-	res.status(200).send({
-		message: "Got the report!",
-		report: newReport
+	var origin = req.get('origin');
+	console.log("DEVLOG: Reports POST request from: " + origin);
+	var input = req.body;
+	//Get an array of all reports from DB
+	var GetAllReportsPromise = new Promise((resolve,reject) => {
+		allReports = GetAllReports();
+		setTimeout(()=>{
+			resolve(allReports);
+		}, 250);
 	});
-  }, () => {
-	  //Handle validation rejection
-	  res.status(400).json({
-		  errcode: "BADINPUT",
-		  message: "Input validation failed.",
-	  });
-  });
+	GetAllReportsPromise.then(reports => {
+		//Send input for validation before doing anything else
+		var inputValidated = new Promise((resolve,reject) => {
+		  var isValidated = validateInput(input, reports);
+		  if (isValidated) {
+			  resolve(input);
+		  } else {
+			  reject();
+		  }
+		});
+		inputValidated.then(vInput => {
+			//Process the validated input
+			var newHeat = 0;
+			if (vInput.heat == 0 || vInput.heat == null) {
+				//heat should be set to value in latest report
+			} else {
+				newHeat = vInput.heat;
+			}
+			var newReport = {
+				cold: vInput.cold,
+				hot: vInput.hot,
+				heat: vInput.heat,
+				elec: vInput.elec,
+				isHeating: vInput.isHeating,
+				isApproved: false,
+			};
+			//Set "nr" attribute
+			//Set approve token
+			newReport.approveToken = rs.generate();
+			console.log(newReport);
+			res.status(200).send({
+				message: "Got the report!",
+				report: newReport
+			});
+		}, () => {
+		  //Handle validation rejection
+		  res.status(400).json({
+			  errcode: "BADINPUT",
+			  message: "Input validation failed.",
+		  });
+		});
+	});
 });
 
 //Validate input data
-function validateInput() {
-	return true;
+function validateInput(input, oldReports) {
+	var lastReport = oldReports[oldReports.length - 1];
+	if (lastReport.cold < input.cold || lastReport.hot < input.hot || lastReport.heat < input.heat || lastReport.elec < input.elec || input.cold==0 || input.hot==0 || input.heat==0 || input.elec==0) {
+		return false;
+	} else {
+		return true;
+	}
 }
 
 module.exports = router;
