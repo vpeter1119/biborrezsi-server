b0f5072a4e708a7e0a24f0e08ea8b28ef8771162
vpeter1119 <vpeter1119@gmail.com>
2020-02-24 21:36:30 +0100

    .

--- src/api/reports.js
+++ src/api/reports.js
@@ -116,7 +116,7 @@ function validateInput(input, oldReports) {
 	console.log(input);
 	console.log("Comparing input to old report:");
 	console.log(lastReport);
-	console.log(lastReport.cold < input.cold + lastReport.hot < input.hot +| lastReport.heat < input.heat + lastReport.elec < input.elec + input.cold==0 + input.hot==0 + input.heat==0 + input.elec==0);
+	console.log(lastReport.cold < input.cold + lastReport.hot < input.hot + lastReport.heat < input.heat + lastReport.elec < input.elec + input.cold==0 + input.hot==0 + input.heat==0 + input.elec==0);
 	if (lastReport.cold < input.cold || lastReport.hot < input.hot || lastReport.heat < input.heat || lastReport.elec < input.elec || input.cold==0 || input.hot==0 || input.heat==0 || input.elec==0) {
 		console.log("Validation failed.");
 		return false;
