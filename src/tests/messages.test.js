f48e12591e66f607fdb2648e663da92f11ebc719
Peter Vertesi <vpeter1119@gmail.com>
2020-09-08 15:13:02 +0200

    Refine messages test

--- src/tests/messages.test.js
+++ src/tests/messages.test.js
@@ -1,10 +1,10 @@
 const messages = require('../local_modules/messages');
 
 describe('messages.SendTestMsg', () => {
-    it('should return an info object', () => {
+    it('should return an info object with a non-empty accepted property', () => {
 
         messages.SendTestMsg().then(info => {
-            expect(info).not.toBe(null);
+            expect(info.accepted).not.toBe(false);
         });
         
     });
